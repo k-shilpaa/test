bef66bc40fd9818e1c7925650c64661ce5764306
k-shilpaa <shilpasreek11@gmail.com>
2022-06-02 17:51:29 +0530

    new changes

--- src/com/calc/Calculator.java
+++ src/com/calc/Calculator.java
@@ -1,9 +1,21 @@
 package com.calc;
 
-public interface Calculator {
-    public int  add(int a,int b);
-    public int subtract(int a,int b);
-    public int multiply(int a,int b);
-    public int divide(int a,int b);
+public abstract class Calculator {
+    public abstract int add(int a, int b);
+
+    public abstract int subtract(int a, int b);
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            return -1;
+        } else {
+            return a / b;
+        }
+
+    }
 }
 
--- src/com/calc/CalculatorImpl.java
+++ src/com/calc/CalculatorImpl.java
@@ -1,6 +1,6 @@
 package com.calc;
 
-public class CalculatorImpl implements Calculator {
+public class CalculatorImpl extends Calculator {
 
 
     @Override
@@ -13,21 +13,7 @@ public class CalculatorImpl implements Calculator {
         return a - b;
     }
 
-    @Override
-    public int multiply(int a, int b) {
-        return a * b;
-    }
 
-    @Override
-    public int divide(int a, int b) {
-
-        if (b == 0) {
-            return -1;
-        } else {
-            return a / b;
-        }
-
-
-    }
 }
+
 
