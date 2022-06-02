92fa9d90f72649e5a46820953039ec0514d3083a
k-shilpaa <shilpasreek11@gmail.com>
2022-06-02 17:29:13 +0530

    added changes

--- src/com/calc/main.java
+++ src/com/calc/main.java
@@ -2,10 +2,10 @@ package com.calc;
 
 public class main {
     public static void main(String[] args) {
-        CalculatorImpl newCalculator=new CalculatorImpl();
-         System.out.println(newCalculator.add(4,5));
-        System.out.println(newCalculator.subtract(7,11));
-        System.out.println(newCalculator.multiply(1,2));
-        System.out.println(newCalculator.divide(4,0));
+        CalculatorImpl newCalculator = new CalculatorImpl();
+        System.out.println(newCalculator.add(4, 5));
+        System.out.println(newCalculator.subtract(7, 11));
+        System.out.println(newCalculator.multiply(1, 2));
+        System.out.println(newCalculator.divide(4, 0));
     }
 }
