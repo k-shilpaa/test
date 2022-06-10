66ddfdefad054a9d8e84da634a3401b852ea28cd
k-shilpaa <shilpasreek11@gmail.com>
2022-06-10 12:23:32 +0530

    matrix

---
+++ src/com/shilp/exercise/matrix.java
@@ -0,0 +1,17 @@
+package com.shilp.exercise;
+
+public class matrix {
+    public static void main(String[] args){
+        int [][] mymatrix = {{1,2,3,4,5,22},{6,7,8,9,10},{11,12,13,14,15,19}};
+        for (int i=0;i< mymatrix.length;i++){
+            for(int j=0;j<mymatrix[i].length;j++){
+
+                    System.out.print(mymatrix[i][j] + " ");
+                }
+               System.out.println();
+
+                }
+            }
+        }
+
+
