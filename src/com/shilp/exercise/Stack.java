989a32a77912e15321c762ab90f9cf092ae83499
k-shilpaa <shilpasreek11@gmail.com>
2022-06-27 12:35:15 +0530

    stack using singlyLinkedList

---
+++ src/com/shilp/exercise/Stack.java
@@ -0,0 +1,65 @@
+package com.shilp.exercise;
+
+public class Stack {
+    public StackNode root;
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.pushdata(5);
+        stack.pushdata(12);
+        stack.pushdata(11);
+        System.out.println(stack.pop());
+        System.out.println(stack.peak());
+        System.out.println(stack.pop());
+        System.out.println(stack.peak());
+        System.out.println(stack.pop());
+        System.out.println(stack.peak());
+
+
+        //System.out.println(stack.peak());
+        //System.out.println(stack.isEmpty());
+
+    }
+
+    public boolean isEmpty() {
+        if (root == null) {
+            return true;
+        }
+        return false;
+    }
+
+    public void pushdata(int data) {
+        StackNode node = new StackNode(data);
+        if (root == null) {
+            root = node;
+        } else {
+            StackNode temp = root;
+            root = node;
+            node.next = temp;
+        }
+        System.out.println(data);
+    }
+
+    public int peak() {
+
+        if (root == null) {
+            throw new RuntimeException("empty stack");
+        } else {
+            return root.data;
+        }
+
+    }
+    public int pop(){
+        if(root==null){
+            return -1;
+        }
+        else {
+            int pop=root.data;
+            root=root.next;
+            return pop;
+
+        }
+
+    }
+
+}
---
+++ src/com/shilp/exercise/StackNode.java
@@ -0,0 +1,9 @@
+package com.shilp.exercise;
+
+public class StackNode {
+    int data;
+    StackNode next;
+    public StackNode(int data){
+        this.data=data;
+    }
+}
