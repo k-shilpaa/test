bf61bf75fbe9fb2bee5c09bedc522f209f41903f
k-shilpaa <shilpasreek11@gmail.com>
2022-06-27 19:47:09 +0530

    stack using linkedlist

--- src/com/shilp/exercise/Stack.java
+++ src/com/shilp/exercise/Stack.java
@@ -2,12 +2,14 @@ package com.shilp.exercise;
 
 public class Stack {
     public StackNode root;
+    private int size = 0;
 
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.pushdata(5);
         stack.pushdata(12);
         stack.pushdata(11);
+        System.out.println(stack.size());
         System.out.println(stack.pop());
         System.out.println(stack.peak());
         System.out.println(stack.pop());
@@ -16,8 +18,8 @@ public class Stack {
         System.out.println(stack.peak());
 
 
-        //System.out.println(stack.peak());
-        //System.out.println(stack.isEmpty());
+        System.out.println(stack.peak());
+        System.out.println(stack.isEmpty());
 
     }
 
@@ -37,6 +39,7 @@ public class Stack {
             root = node;
             node.next = temp;
         }
+        size++;
         System.out.println(data);
     }
 
@@ -49,17 +52,22 @@ public class Stack {
         }
 
     }
-    public int pop(){
-        if(root==null){
+
+    public int pop() {
+        if (root == null) {
             return -1;
-        }
-        else {
-            int pop=root.data;
-            root=root.next;
+        } else {
+            int pop = root.data;
+            root = root.next;
+            size--;
             return pop;
 
         }
 
+    }
+
+    public int size() {
+        return size;
     }
 
 }
